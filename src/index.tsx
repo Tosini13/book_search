959dc2a66a5ea0cb8b126f9a580acd54b2271cca
Tosini13 <jbartos13@gmail.com>
2023-11-06 11:34:18 +0900

    feat: provide theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = {
+  color: {
+    primary: "#0a0b1a",
+    success: "green",
+    error: "red",
+  },
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
